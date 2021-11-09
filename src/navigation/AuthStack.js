93faf3bc4de27062aeb9a6531fe08f9f66332532
Tucker2015 <kevtucker@gmail.com>
2021-11-09 20:16:44 +0000

    - Initial Commit

---
+++ src/navigation/AuthStack.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignupScreen from '../screens/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthStack() {
+
+    return (
+        <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+            <Stack.Screen name="Signup" component={SignupScreen} />
+        </Stack.Navigator>
+    )
+}
